fea9617d67bd5caaefeb938b871ceb97771ef843
unknown <brahmamanumukonda000@gmail.com>
2022-04-11 20:19:32 +0530

    assignment-4

--- Assignment-4/code/src/Post/Post.js
+++ Assignment-4/code/src/Post/Post.js
@@ -2,8 +2,10 @@ import PostFooter  from "./PostFooter";
 import  PostHeader from "./PostHeader";
 import  PostImage  from "./PostImage";
 
+
 const Post = ({config}) =>{
     return <div class="PostContainer">
+        
         <PostHeader config={config}/>
         <PostImage config={config}/>
         <PostFooter config={config}/>
